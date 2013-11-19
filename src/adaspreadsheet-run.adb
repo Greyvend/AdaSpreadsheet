e728a1e1fd2f2509f16855923b0d12894850dd60
Serge Mosin <svmosin@gmail.com>
2013-11-19 22:48:47 +0700

    first commit, adding new source and meta files

---
+++ src/adaspreadsheet-run.adb
@@ -0,0 +1,6 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure AdaSpreadsheet.Run is
+begin
+   Put_Line ("ну и че?!");
+end AdaSpreadsheet.Run;
---
+++ src/adaspreadsheet.ads
@@ -0,0 +1,2 @@
+package AdaSpreadsheet is
+end AdaSpreadsheet;
